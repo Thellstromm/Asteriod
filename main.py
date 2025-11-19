49bf3cfd19951078314b17c76f82ecaf4bb93543
Thellstromm <tellstrom.ludwig@icloud.com>
2025-11-19 12:27:35 +0100

    2 commit

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from logger import log_state
 
 def main():
      
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init() # Initialize pygame
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Set screen size
 
-    while True:
-        log_state()
-        for event in pygame.event.get():
+    clock = pygame.time.Clock()
+    dt = 0
+
+
+    while True: # Main game loop
+        log_state() # Log the game state
+        for event in pygame.event.get(): #
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
+        screen.fill("black") # Clear the screen with black color
+        pygame.display.flip() # Update the full display surface to the screen
+        dt = clock.tick(60) / 1000  # Amount of seconds between each loop
+         # print(dt)
 
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
